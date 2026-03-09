65bad0742410d345b439c73253ac27158621cc46
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-03-08 19:46:34 -0500

    checkpoint 3

--- 100DaysOfSwift.playground/Pages/Checkpoint 3.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwift.playground/Pages/Checkpoint 3.xcplaygroundpage/Contents.swift
@@ -2,6 +2,14 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
