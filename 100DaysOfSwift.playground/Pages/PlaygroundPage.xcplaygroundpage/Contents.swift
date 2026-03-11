3e7e3faa5efb4b0507597a3b5e1b7fe4a78f5cc6
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-03-10 20:12:29 -0500

    completed checkpoint 4

---
+++ 100DaysOfSwift.playground/Pages/PlaygroundPage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+func printTimesTables(number: Int) {
+    for i in 1...12 {
+        print("\(i) * \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5)
--- 100DaysOfSwift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-var arrayOfStrings = ["String", "test", "test", "Item", "Stores", "here", "seven"]
-
-var uniqueStringsSet = Set(arrayOfStrings)
-
-print(arrayOfStrings.count, uniqueStringsSet.count, "unique count")
